82008404b6363bf3acc6ffb207e623f20755f3a3
Simão Paiva <55535509+SimonLimon@users.noreply.github.com>
2022-04-24 20:10:03 +0000

    hosting implemented with uptimeRobot

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
 import metro
 from datetime import datetime
-import discord
 from discord.ext import commands
+from webserver import keep_alive
 
-
-bot = commands.Bot(command_prefix = "!")
+bot = commands.Bot(command_prefix="!")
 
 metroToken = metro.generate_token()
 lastTime = datetime.now()
 
 def updateToken():
-  global metroToken
-  global lastTime 
-  now = datetime.now()
-  print(now)
-  if(lastTime != None and (now - lastTime).total_seconds() > 3600):
-    metroToken = metro.generate_token()
-    lastTime = now
-  if(lastTime == None):
-    lastTime = now
-  print(metroToken)
-  return metroToken
-  
+    global metroToken
+    global lastTime
+    now = datetime.now()
+    print(now)
+    if (lastTime != None and (now - lastTime).total_seconds() > 3600):
+        metroToken = metro.generate_token()
+        lastTime = now
+    if (lastTime == None):
+        lastTime = now
+    print(metroToken)
+    return metroToken
+
 
 def isMetroOpen():
-  return True
+    return True
 
 
 @bot.event
 async def on_start():
-  print("Bot is online")
+    print("Bot is online")
 
 
 @bot.command()
 async def info(ctx):
-  res = ""
-  for id in metro.station_codes():
-    res = res + id + ", "
-  await ctx.send("commands\n" + "!getTrip `ID da estacao`" + 
-                 "info" + "All station ids: " + res)
+    res = ""
+    for id in metro.station_codes():
+        res = res + id + ", "
+    await ctx.send("commands\n" + "!getTrip `ID da estacao`" + "info" +
+                   "All station ids: " + res)
 
 
 @bot.command()
 async def getTrip(ctx, arg1):
-  if(isMetroOpen):
-    await ctx.send(metro.request_time(arg1, updateToken()))
-  else:
-    await ctx.send("Metro is closed reopens at 6.30h")
+    if (isMetroOpen):
+        await ctx.send(metro.request_time(arg1, updateToken()))
+    else:
+        await ctx.send("Metro is closed reopens at 6.30h")
 
 
 @bot.command()
 async def getToken(ctx):
-  await ctx.send(updateToken())
-  
+    await ctx.send(updateToken())
 
-token=("OTY3MTkzMTY0ODQ4OTg4MzAw.YmMu1A.WpmuVSHnR4vGzMjYAU8SrAB_ORI")
+
+keep_alive()
+
+token = ("OTY3MTkzMTY0ODQ4OTg4MzAw.YmMu1A.mY0eOuYLEjP3dmhcnUsW6sQSaT8")
 bot.run(token)
